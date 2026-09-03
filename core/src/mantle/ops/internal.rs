3faca3c6ffdc27c1f53a81c174889569606de54f
Álex <alex93cabeza@gmail.com>
2026-09-03 10:11:48 +0000

    refactor(operation): op code into trait (#3308)

--- core/src/mantle/ops/internal.rs
+++ core/src/mantle/ops/internal.rs
@@ -4,36 +4,39 @@ use super::{
     Op,
     channel::{config::ChannelConfigOp, deposit::DepositOp, inscribe::InscriptionOp},
     leader_claim::LeaderClaimOp,
-    op_codes,
     sdp::{SDPActiveOp, SDPDeclareOp, SDPWithdrawOp},
     serde_::OpWire,
     transfer::TransferOp,
 };
-use crate::mantle::ops::{
-    channel::{channel_transfer::ChannelTransferOp, withdraw::ChannelWithdrawOp},
-    pow::ClaimPowRewardOp,
+use crate::mantle::{
+    ledger::ProvableOperation as _,
+    ops::{
+        channel::{channel_transfer::ChannelTransferOp, withdraw::ChannelWithdrawOp},
+        pow::ClaimPowRewardOp,
+    },
 };
 
 /// Core set of supported Mantle operations and their serialization behaviour.
 #[derive(Serialize)]
 #[serde(untagged)]
 pub enum OpSer<'a> {
-    ChannelInscribe(OpWire<{ op_codes::INSCRIBE }, &'a InscriptionOp>),
-    ChannelConfig(OpWire<{ op_codes::CHANNEL_CONFIG }, &'a ChannelConfigOp>),
-    ChannelDeposit(OpWire<{ op_codes::CHANNEL_DEPOSIT }, &'a DepositOp>),
-    ChannelWithdraw(OpWire<{ op_codes::CHANNEL_WITHDRAW }, &'a ChannelWithdrawOp>),
-    ChannelTransfer(OpWire<{ op_codes::CHANNEL_TRANSFER }, &'a ChannelTransferOp>),
-    SDPDeclare(OpWire<{ op_codes::SDP_DECLARE }, &'a SDPDeclareOp>),
-    SDPWithdraw(OpWire<{ op_codes::SDP_WITHDRAW }, &'a SDPWithdrawOp>),
-    SDPActive(OpWire<{ op_codes::SDP_ACTIVE }, &'a SDPActiveOp>),
-    LeaderClaim(OpWire<{ op_codes::LEADER_CLAIM }, &'a LeaderClaimOp>),
-    Transfer(OpWire<{ op_codes::TRANSFER }, &'a TransferOp>),
-    ClaimPowReward(OpWire<{ op_codes::CLAIM_POW_REWARD }, &'a ClaimPowRewardOp>),
+    Transfer(OpWire<{ TransferOp::CODE }, &'a TransferOp>),
+    ChannelInscribe(OpWire<{ InscriptionOp::CODE }, &'a InscriptionOp>),
+    ChannelConfig(OpWire<{ ChannelConfigOp::CODE }, &'a ChannelConfigOp>),
+    ChannelDeposit(OpWire<{ DepositOp::CODE }, &'a DepositOp>),
+    ChannelWithdraw(OpWire<{ ChannelWithdrawOp::CODE }, &'a ChannelWithdrawOp>),
+    ChannelTransfer(OpWire<{ ChannelTransferOp::CODE }, &'a ChannelTransferOp>),
+    SDPDeclare(OpWire<{ SDPDeclareOp::CODE }, &'a SDPDeclareOp>),
+    SDPWithdraw(OpWire<{ SDPWithdrawOp::CODE }, &'a SDPWithdrawOp>),
+    SDPActive(OpWire<{ SDPActiveOp::CODE }, &'a SDPActiveOp>),
+    LeaderClaim(OpWire<{ LeaderClaimOp::CODE }, &'a LeaderClaimOp>),
+    ClaimPowReward(OpWire<{ ClaimPowRewardOp::CODE }, &'a ClaimPowRewardOp>),
 }
 
 impl<'a> From<&'a Op> for OpSer<'a> {
     fn from(value: &'a Op) -> Self {
         match value {
+            Op::Transfer(op) => Self::Transfer(OpWire::new(op)),
             Op::ChannelInscribe(op) => Self::ChannelInscribe(OpWire::new(op)),
             Op::ChannelConfig(op) => Self::ChannelConfig(OpWire::new(op)),
             Op::ChannelDeposit(op) => Self::ChannelDeposit(OpWire::new(op)),
@@ -43,7 +46,6 @@ impl<'a> From<&'a Op> for OpSer<'a> {
             Op::SDPWithdraw(op) => Self::SDPWithdraw(OpWire::new(op)),
             Op::SDPActive(op) => Self::SDPActive(OpWire::new(op)),
             Op::LeaderClaim(op) => Self::LeaderClaim(OpWire::new(op)),
-            Op::Transfer(op) => Self::Transfer(OpWire::new(op)),
             Op::ClaimPowReward(op) => Self::ClaimPowReward(OpWire::new(op)),
         }
     }
@@ -53,24 +55,25 @@ impl<'a> From<&'a Op> for OpSer<'a> {
 #[derive(Deserialize)]
 #[serde(untagged)]
 pub enum OpDe {
-    ChannelInscribe(OpWire<{ op_codes::INSCRIBE }, InscriptionOp>),
-    ChannelConfig(OpWire<{ op_codes::CHANNEL_CONFIG }, ChannelConfigOp>),
-    ChannelDeposit(OpWire<{ op_codes::CHANNEL_DEPOSIT }, DepositOp>),
-    ChannelWithdraw(OpWire<{ op_codes::CHANNEL_WITHDRAW }, ChannelWithdrawOp>),
-    ChannelTransfer(OpWire<{ op_codes::CHANNEL_TRANSFER }, ChannelTransferOp>),
-    SDPDeclare(OpWire<{ op_codes::SDP_DECLARE }, SDPDeclareOp>),
-    SDPWithdraw(OpWire<{ op_codes::SDP_WITHDRAW }, SDPWithdrawOp>),
-    SDPActive(OpWire<{ op_codes::SDP_ACTIVE }, SDPActiveOp>),
-    LeaderClaim(OpWire<{ op_codes::LEADER_CLAIM }, LeaderClaimOp>),
-    Transfer(OpWire<{ op_codes::TRANSFER }, TransferOp>),
-    ClaimPoWReward(OpWire<{ op_codes::CLAIM_POW_REWARD }, ClaimPowRewardOp>),
+    Transfer(OpWire<{ TransferOp::CODE }, TransferOp>),
+    ChannelConfig(OpWire<{ ChannelConfigOp::CODE }, ChannelConfigOp>),
+    ChannelInscribe(OpWire<{ InscriptionOp::CODE }, InscriptionOp>),
+    ChannelDeposit(OpWire<{ DepositOp::CODE }, DepositOp>),
+    ChannelWithdraw(OpWire<{ ChannelWithdrawOp::CODE }, ChannelWithdrawOp>),
+    ChannelTransfer(OpWire<{ ChannelTransferOp::CODE }, ChannelTransferOp>),
+    SDPDeclare(OpWire<{ SDPDeclareOp::CODE }, SDPDeclareOp>),
+    SDPWithdraw(OpWire<{ SDPWithdrawOp::CODE }, SDPWithdrawOp>),
+    SDPActive(OpWire<{ SDPActiveOp::CODE }, SDPActiveOp>),
+    LeaderClaim(OpWire<{ LeaderClaimOp::CODE }, LeaderClaimOp>),
+    ClaimPowReward(OpWire<{ ClaimPowRewardOp::CODE }, ClaimPowRewardOp>),
 }
 
 impl From<OpDe> for Op {
     fn from(value: OpDe) -> Self {
         match value {
-            OpDe::ChannelInscribe(w) => Self::ChannelInscribe(w.into_op()),
+            OpDe::Transfer(w) => Self::Transfer(w.into_op()),
             OpDe::ChannelConfig(w) => Self::ChannelConfig(w.into_op()),
+            OpDe::ChannelInscribe(w) => Self::ChannelInscribe(w.into_op()),
             OpDe::ChannelDeposit(w) => Self::ChannelDeposit(w.into_op()),
             OpDe::ChannelWithdraw(w) => Self::ChannelWithdraw(w.into_op()),
             OpDe::ChannelTransfer(w) => Self::ChannelTransfer(w.into_op()),
@@ -78,8 +81,7 @@ impl From<OpDe> for Op {
             OpDe::SDPWithdraw(w) => Self::SDPWithdraw(w.into_op()),
             OpDe::SDPActive(w) => Self::SDPActive(w.into_op()),
             OpDe::LeaderClaim(w) => Self::LeaderClaim(w.into_op()),
-            OpDe::Transfer(w) => Self::Transfer(w.into_op()),
-            OpDe::ClaimPoWReward(w) => Self::ClaimPowReward(w.into_op()),
+            OpDe::ClaimPowReward(w) => Self::ClaimPowReward(w.into_op()),
         }
     }
 }
